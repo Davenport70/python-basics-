c94a921f307b52fe9f18b85ba44465f83d6b1451
Zack Davenport <zackdavenport@tcadmins-MacBook-Air.local>
2020-03-03 11:10:31 +0000

    learning about variables, types and print

---
+++ 101_python_variables_print_type.py
@@ -0,0 +1,20 @@
+# Variables
+# it is like a box, you give it a name and you put stuff inside of it.
+# defining variable book
+book = "Rich Dad Poor Dad"
+
+# print function
+# outputs content to the terminal (makes it visible)
+
+book
+print(book)
+
+# type function
+# allows us to check data types.
+# use refractor to change all the variable names
+
+data_type_of_book = type(book)
+print(data_type_of_book)
+
+## Conventions
+# lower case naming for variables with under score
